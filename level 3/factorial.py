d9652f2d1714b0cba398a41e8a552b048a53f737
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-15 22:58:23 +0500

    calculate factorial

---
+++ level 3/factorial.py
@@ -0,0 +1,17 @@
+# Factorial Calculation
+# Problem: Write a function that calculates the factorial of a given number.
+# Input: 5
+# Output: 120
+# Hint: Factorial of n (denoted as n!) is the product of all positive integers less than or equal to n.
+
+
+
+
+
+
+
+# calculate_factoroial(num)
+# factorial = num * num-1
+# sum
+# for i in range(num-1):
+# factorial += 
