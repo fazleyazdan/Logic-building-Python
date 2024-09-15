ce4e3e4dee332305f99997dc16ac66063815756b
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-15 23:17:34 +0500

    check if prime

--- level 2/prime_numbers.py
+++ level 2/prime_numbers.py
@@ -3,3 +3,21 @@
 # Input: 17
 # Output: True
 # Hint: A prime number is a number greater than 1 that has no divisors other than 1 and itself.
+
+
+def check_prime(num):           
+    
+    if num ==0 or num == 1:
+        return False
+    elif num%2 == 0 or num%3 == 0:
+        return False
+    else:
+        return True
+        
+print(check_prime(7))
+print(check_prime(8))
+print(check_prime(0))
+print(check_prime(11))
+print(check_prime(17))
+
+
