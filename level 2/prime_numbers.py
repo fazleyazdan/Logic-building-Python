c686feb201d5ddda48acaf8ba13236b3c541c71e
fazleyazdan <fazleyazdan345@gmail.com>
2024-09-15 22:58:50 +0500

    prime numbers

---
+++ level 2/prime_numbers.py
@@ -0,0 +1,5 @@
+# Prime Number Check
+# Problem: Write a function that checks if a given number is a prime number.
+# Input: 17
+# Output: True
+# Hint: A prime number is a number greater than 1 that has no divisors other than 1 and itself.
